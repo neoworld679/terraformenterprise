a35d3ee57604adc62b908e57cc6789dfb55d5905
F N <fadinaja@gmail.com>
2018-07-28 01:44:12 +0100

    added env

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "test" {
 }
 
 resource "azurerm_subnet" "test" {
-  name                 = "acctsub"
+  name                 = "${var.environment}-acctsub"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "10.0.2.0/24"
