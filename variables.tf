7d7971b805f7fa029271b4d53a4f80eba360512b
F N <fadinaja@gmail.com>
2018-07-31 01:03:27 +0100

    added variables.tf left only RG

---
+++ variables.tf
@@ -0,0 +1,4 @@
+Subscription_id = 30c7852b-e63a-4c26-ba25-1a6463462c14
+Tenant_ID = 46dd0b7d-254f-49b4-9447-cfdc312fe497
+Client_id = bfbd9828-be71-4fc9-9282-5f8eecf880ac
+Client_secret = irB01fwU90169L07dJHC2IQlTI/oBXiKLEo0f86rmiE=
