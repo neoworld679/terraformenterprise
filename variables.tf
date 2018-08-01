81705fde171bcb0d9037c272e261edf8bcdeca44
F N <fadinaja@gmail.com>
2018-08-01 01:29:54 +0100

    edit variable even more

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+default = ""
+}
+
+variable "subscription_id" {
+default = "30c7852b-e63a-4c26-ba25-1a6463462c14"
+}
+
+variable "tenant_id" {
+default = "46dd0b7d-254f-49b4-9447-cfdc312fe497"
+}
